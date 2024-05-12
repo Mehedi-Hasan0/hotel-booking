9f02c37bf49b555fd851b8d96fa1fa95e1e921fe
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-12 22:25:15 +0600

    [ADD] Date picker component added

--- src/components/shared/search/SearchWithDate.tsx
+++ src/components/shared/search/SearchWithDate.tsx
@@ -1,4 +1,5 @@
-import { IoBedOutline } from "react-icons/io5";
+import { RxCalendar } from "react-icons/rx";
+import DatePicker from "./DatePicker";
 
 export default function SearchWithDate() {
   return (
@@ -8,17 +9,11 @@ export default function SearchWithDate() {
         className="w-full h-[54px] lg:h-14 bg-white rounded-md flex items-center gap-2 pl-4 lg:pl-2"
       >
         {/* icon */}
-        <IoBedOutline
+        <RxCalendar
           size={28}
-          className="text-secondary-foreground cursor-pointer w-6 h-6 lg:w-7 lg:h-7"
+          className="text-secondary-foreground cursor-pointer w-5 h-5 lg:w-6 lg:h-6"
         />
-        <input
-          type="search"
-          //   name="places"
-          //   id="places"
-          className="w-full outline-none rounded-md placeholder:text-sm placeholder:text-secondary placeholder:font-medium focus:placeholder:text-secondary-foreground"
-          placeholder="Search Date"
-        />
+        <DatePicker />
       </label>
       {/* search place optionns
       <div className="absolute top-16 w-[400px] h-[200px] bg-white rounded-md border border-gray-400"></div> */}
