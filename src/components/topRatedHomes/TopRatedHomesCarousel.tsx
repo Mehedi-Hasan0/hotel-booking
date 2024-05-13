82fa9bcf4d3205b0845738f89c4d7ccac2974ec2
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-13 22:10:18 +0600

    [ADD] Single hotel link added

--- src/components/topRatedHomes/TopRatedHomesCarousel.tsx
+++ src/components/topRatedHomes/TopRatedHomesCarousel.tsx
@@ -15,7 +15,7 @@ SwiperCore.use([Navigation]);
 const TopRatedHomesCarousel = () => {
   const [swiperLoaded, setSwiperLoaded] = useState(false);
   const { isLoading, hotels } = useDataContext();
-  const [swiper, setSwiper] = useState<SwiperCore | null>(null);
+  // const [swiper, setSwiper] = useState<SwiperCore | null>(null);
   const [slidePerView, setSlidePerView] = useState(
     typeof window !== "undefined"
       ? window.innerWidth < 640
@@ -55,21 +55,21 @@ const TopRatedHomesCarousel = () => {
     });
   });
 
-  const handlePrevSlide = () => {
-    if (swiper) {
-      swiper.slidePrev();
-    }
-  };
+  // const handlePrevSlide = () => {
+  //   if (swiper) {
+  //     swiper.slidePrev();
+  //   }
+  // };
 
-  const handleNextSlide = () => {
-    if (swiper) {
-      swiper.slideNext();
-    }
-  };
+  // const handleNextSlide = () => {
+  //   if (swiper) {
+  //     swiper.slideNext();
+  //   }
+  // };
 
-  const handleSwiperInit = (swiper: SwiperCore) => {
-    setSwiper(swiper);
-  };
+  // const handleSwiperInit = (swiper: SwiperCore) => {
+  //   setSwiper(swiper);
+  // };
 
   if (!swiperLoaded && isLoading) {
     return null; // loading state
@@ -79,13 +79,13 @@ const TopRatedHomesCarousel = () => {
     <div className="relative">
       {/* left slider btn */}
       <p
-        onClick={handlePrevSlide}
+        // onClick={handlePrevSlide}
         className="absolute top-[40%] left-0 md:-left-5 z-10 flex items-center justify-center w-7 h-7 lg:w-9 lg:h-9 rounded-full bg-white shadow-lg border cursor-pointer"
       >
         <MdKeyboardArrowLeft size={24} className="text-secondary w-5 h-5" />
       </p>
       <Swiper
-        onSwiper={handleSwiperInit}
+        // onSwiper={handleSwiperInit}
         slidesPerView={slidePerView}
         spaceBetween={30}
         loop={true}
@@ -107,7 +107,7 @@ const TopRatedHomesCarousel = () => {
       </Swiper>
       {/* right slider btn */}
       <p
-        onClick={handleNextSlide}
+        // onClick={handleNextSlide}
         className="absolute top-[40%] right-0 md:-right-5 z-10 flex items-center justify-center w-7 h-7 lg:w-9 lg:h-9 rounded-full bg-white shadow-lg border cursor-pointer"
       >
         <MdKeyboardArrowRight size={24} className="text-secondary w-5 h-5" />
